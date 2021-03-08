50e045f359e443c447fd03114a29c8c3dbedf610
TheRashRash <rasheed.gudal@gmail.com>
2021-03-08 11:09:00 +0000

    initial commit

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-const wordSet_1 = []
-const wordSet_2 = []
-const wordSet_3 = []
-const wordSet_4 = []
-const wordSet_5 = []
-const wordSet_6 = []
+const randomWord = require('random-word')
 
-const randIdx = Math.floor(Math.random()*5)
-console.log(wordSet_1[randIdx] +' '+ wordSet_2[randIdx]+' '+ wordSet_3[randIdx]+' '+wordSet_4[randIdx]) 
+const randIdx = Math.floor(Math.random()*50)
+// console.log(wordSet_1[randIdx] +' '+ wordSet_2[randIdx]+' '+ wordSet_3[randIdx]+' '+wordSet_4[randIdx]) 
+let wordArray = []
+
+const adjectives = []
+const verbs = []
+const nouns = []
+
+let i = 0
+while(i <= 50){
+    let word = randomWord(randIdx)
+    switch(word){
+        case (/ed$/ || /es$/):
+            verbs.push(word)
+            break
+        case (/ing$/):
+            adjectives.push(word)
+            break
+        default:
+            nouns.push(word)
+    }
+    i++
+}
+
+console.log(randomWord(randIdx))
+/* 
+To do:
+1) Using randomWord, sort words into adjectives, nouns, verbs, adverbs and conjunctives
+2) 
+*/
